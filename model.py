29800f956e537d65966cf2f51c457db967ff210a
Lauren Gordon-Fahn <laurengordonfahn@gmail.com>
2017-02-20 11:48:58 -0800

    Added outline for DB Model
    Outlined Model to hold Todo and User
    Outlined User to have facebook sign-in

---
+++ model.py
@@ -0,0 +1,52 @@
+from flask_sqlalchemy import SQLAlchemy
+#python World Time Zone
+import pytz 
+#for onupdate
+import datetime
+
+db = SQLAlchemy()
+
+def connect_to_db(app, url = 'postgresql:///todolist'):
+    """ Connect the database to our Flask app. """
+    app.config['SQLALCHEMY_DATABASE_URI'] = url
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.app = app
+    db.init_app(app)
+    app.config['SQLALCHEMY_ECHO'] = True
+
+
+class AwareDateTime(db.TypeDecorator):
+    """ Results returned as aware datetimes, not naive ones.
+    sourced from: 
+    http://stackoverflow.com/questions/23316083/sqlalchemy-how-to-load-dates-with-timezone-utc-dates-stored-without-timezone
+    """
+    impl = db.DateTime
+    def process_result_value(self, value, dialect):
+        return value.replace(tzinfo=pytz.utc)
+
+
+class User(db.Model):
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(150), nullable=False)
+    facebook_id = db.Column(db.String(30), nullable=False, unique=True)
+
+
+class Todo(db.Model):
+    __tablename__ = "todos"
+
+    id = db.Column(db.Integer, primary_key = True,
+                                autoincrement = True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    conent = db.Column(db.Text, nullable=False)
+    created_at = db.Column(AwareDateTime, default=db.func.now(), nullable=False, onupdate-datetime.datetime.now)
+
+
+
+if __name__ == "__main__":
+    from server import app
+    connect_to_db(app)
+    db.create_all()
